7389cd68660c28732c9d33ee9731e36cffb71663
NVombat <nvombatkere@gmail.com>
2022-02-21 01:18:35 +0530

    settings and config updates

--- server/core/settings.py
+++ server/core/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o(m@@uebr3i%&)xpd1tifb6ll-c8+v=)%$w^bfxfekzrp9swma'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") else False
+print("DEBUG VALUE:", DEBUG)
+
+USE_DATABASE = "MONGO" if DEBUG is False else "TEST"
+print("USE_DATABASE:", USE_DATABASE)
 
 ALLOWED_HOSTS = []
 
@@ -55,6 +62,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://localhost:8000",
+]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
@@ -74,6 +90,63 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+
+if DEBUG:
+    print("THROTTLE RATE: DEVELOPMENT")
+    REST_FRAMEWORK = {
+        "DEFAULT_THROTTLE_CLASSES": [
+            "rest_framework.throttling.AnonRateThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {
+            "init_throttle": "10/min",
+            "anon": "10/min",
+        },
+        "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+        "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 10,
+    }
+
+else:
+    print("THROTTLE RATE: DEPLOYMENT")
+    REST_FRAMEWORK = {
+        "DEFAULT_THROTTLE_CLASSES": [
+            "rest_framework.throttling.AnonRateThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {
+            "init_throttle": "1000/min",
+            "anon": "10/min",
+        },
+        "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+        "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 10,
+    }
+
+if DEBUG:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+            "LOCATION": "/home/nvombat/Desktop/Django-Template/server/cache",
+        }
+    }
+else:
+    print("Deployment Does Not Support Caching")
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
+if USE_DATABASE == "MONGO":
+    DATABASE = {"mongo_uri": os.getenv("MONGO_URI"), "db": os.getenv("MONGO_DB")}
+    AWS_BUCKET_FOLDER = os.getenv("BUCKET_SUBFOLDER")
+
+elif USE_DATABASE == "TEST":
+    DATABASE = {
+        "mongo_uri": os.getenv("TEST_MONGO_URI"),
+        "db": os.getenv("TEST_MONGO_DB"),
+    }
+    AWS_BUCKET_FOLDER = os.getenv("TEST_BUCKET_SUBFOLDER")
+
+print("USING DB: ", DATABASE["db"])
 
 
 # Database
@@ -116,9 +189,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+if DEBUG:
+    AWS_ACCESS_KEY_ID = os.environ.get("TEST_AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("TEST_AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("TEST_AWS_STORAGE_BUCKET_NAME")
+    AWS_OBJECT_URL_PREFIX = os.getenv("TEST_AWS_S3_OBJECT_URL_PREFIX")
+else:
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    AWS_OBJECT_URL_PREFIX = os.getenv("AWS_S3_OBJECT_URL_PREFIX")
+
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
